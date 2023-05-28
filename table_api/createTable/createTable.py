198e8b94658bad434baec5bdd2153b45d4baf8c4
huzaifamk <malikhuzaifa99@gmail.com>
2023-05-28 14:50:51 +0500

    Added dynamic table naming convention

--- table_api/createTable/createTable.py
+++ table_api/createTable/createTable.py
@@ -1,12 +1,19 @@
 def createTable(conn):
+
+    # import os
+    # Get table name from environment variable
+    # table_name = os.getenv('TABLE_NAME')
+
+    # Or get table name from user input
+    table_name = input('Enter table name: ')
+
     # Create a table
-    conn.execute('''CREATE TABLE IF NOT EXISTS users
+    conn.execute('''CREATE TABLE IF NOT EXISTS {table_name}
                     (id INTEGER PRIMARY KEY AUTOINCREMENT,
                     name TEXT NOT NULL,
                     email TEXT NOT NULL,
                     username TEXT NOT NULL,
                     password TEXT NOT NULL,
                     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-                    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP);''')
-
-    print('Table created successfully.')
+                    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP);'''.format(table_name=table_name))
+    # Close c
